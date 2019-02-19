749cd6d7520a5a9e63e4c2639dfcdc5fa6fbcf70
Derek Duncan <derek.duncan@nupark.com>
2019-02-19 14:27:01 -0500

    Initial project commit

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour 
+{
+	public float tumble;
+
+	void Start()
+	{
+		Rigidbody rigidBody = GetComponent<Rigidbody> ();
+		rigidBody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
